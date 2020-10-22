13c5c0fc3a5840331eaf52d0aba0b18f54fb460a
nidia <nidiacmh@gmail.com>
2020-10-22 18:11:04 -0500

    pipe-slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Nidia Carolina';
+
+  arreglo = [1,2,3,4,5,6,7,8,9];
 }
